417ee5704e5f1f3fccd87618188e029dda905ce5
Toby Wang <tobywzy113@gmail.com>
2026-05-09 10:45:08 +1200

    Chat page layout

--- index.js
+++ index.js
@@ -12,8 +12,19 @@ app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 app.set('views', path.join(dirname, 'views'));
 
+const chat = {
+    users: [],
+    history: []
+
+};
+
 app.post('/join', (req,res) => {
     console.log(req.body.nickname);
+    chat.history.push[{
+        nickname: 'System',
+        message: `Welcome ${req,body.nickname}`,
+        datetime: new Date()
+    }];
     res.render('chat');
 });
 
